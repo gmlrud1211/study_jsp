dbd5b43b95d92c6b58477c9174932e707f9b9c13
gmlrud1211 <gmlrud1211@naver.com>
2019-02-21 21:08:18 +0900

    controller 수정

--- Board/src/controller/board/BoardListController.java
+++ Board/src/controller/board/BoardListController.java
@@ -1,25 +1,50 @@
 package controller.board;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.board.BoardDao;
+import dao.board.BoardDaoImpl;
+import dto.board.Board;
+
 @WebServlet("/board/list")
 public class BoardListController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		//view
-		request.getRequestDispatcher("/view/board/list.jsp").forward(request, response);
-		
-		
-		
-		
+	private BoardDao boardDao = new BoardDaoImpl();
 	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//Board 테이블 전체 조회
+		List<Board> boardList = boardDao.selectAll();
+
+		//view에 전달
+		request.setAttribute("boardList", boardList);
+		
+		
+		RequestDispatcher rd;
+		rd= request.getRequestDispatcher("/view/board/list.jsp");
+		rd.forward(request, response);
+		
+		
+		
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		req.setCharacterEncoding("utf-8");
+		
+
+		
 	}
 
 }
