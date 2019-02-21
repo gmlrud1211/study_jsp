3234594724eb94b29f639672a00f9a1d06370b18
gmlrud1211 <gmlrud1211@naver.com>
2019-02-21 20:26:48 +0900

    BoardDao 수정
    Dao 수정

--- Board/src/dao/board/BoardDao.java
+++ Board/src/dao/board/BoardDao.java
@@ -1,5 +1,19 @@
 package dao.board;
 
+import java.util.List;
+
+import dto.board.Board;
+
 public interface BoardDao {
 
+	
+	//게시글 전체 조회
+	public List SelectAll();
+	
+	//게시글번호로 게시글 보기
+	public Board selectBoardByBoardno(Board board);
+	
+	//추천수 hit+1
+	public void updateHit(Board board);
+	
 }
--- Board/src/dao/board/BoardDaoImpl.java
+++ Board/src/dao/board/BoardDaoImpl.java
@@ -1,7 +1,28 @@
 package dao.board;
 
+import java.util.List;
+
+import dto.board.Board;
+
 public class BoardDaoImpl implements BoardDao {
 
+	@Override
+	public List SelectAll() {
+		return null;
+	}
+
+	@Override
+	public Board selectBoardByBoardno(Board board) {
+		return null;
+	}
+
+	@Override
+	public void updateHit(Board board) {
+		
+	}
+
+
 	
+
 	
 }
